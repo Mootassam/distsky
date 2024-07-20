1ea5e7f5a3f890a5e4d4298aac034fe3bdf519ab
Mootassam <Mootassame@gmail.com>
2024-07-20 15:44:17 +0100

    add the new commit

---
+++ assets/ButtonIcon-90bfcf72.js
@@ -0,0 +1,1 @@
+import{k as n}from"./index-b8509db6.js";const i=s=>s.loading?n.jsx("div",{className:"spinner"}):s.iconClass?n.jsx("i",{className:s.iconClass}):null;export{i as B};
---
+++ assets/LoadingModal-61b8c14d.js
@@ -0,0 +1,1 @@
+import{k as s}from"./index-b8509db6.js";function i(){return s.jsx("div",{className:"loading__page",children:s.jsx("div",{className:"spinner__big"})})}export{i as L};
